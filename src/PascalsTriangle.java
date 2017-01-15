710cf806a15d21a09e3332e312a3ca2d4f24e5ed
kadama <kadama@ccs.neu.edu>
2017-01-14 23:08:57 -0500

    pascals triangle adding variation

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -22,8 +22,30 @@ public class PascalsTriangle {
         return list;
     }
 
+    List<Integer> generateNew(int numRows){
+
+
+        List<Integer> row = new ArrayList<Integer>();
+
+            for(int i = 0; i < numRows+1; i++) {
+                row.add(0, 1);
+                for (int j = 1; j < row.size() - 1; j++) {
+                    row.set(j, row.get(j) + row.get(j + 1));
+                }
+            }
+
+
+        return row;
+    }
+
+
     public static void main(String args[]){
         PascalsTriangle obj = new PascalsTriangle();
+
+        List<Integer> nl = obj.generateNew(3);
+        for(Integer n : nl){
+            System.out.print(n.intValue());
+        }
 
         List<List<Integer>> list = obj.generate(5);
         for(List<Integer> l : list){
